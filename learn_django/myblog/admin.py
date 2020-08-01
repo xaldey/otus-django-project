76a68e1d31ed2b83ab53af6e54bc91a138e68195
xaldey <xaldey@mail.ru>
2020-08-01 16:17:48 +0300

    Стартовая инициализация по окончанию урока.

--- learn_django/myblog/admin.py
+++ learn_django/myblog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Article
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = "id", "first_name", "last_name", "full_name"
+    list_display_links = "full_name", "last_name"
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    def text_short(self, obj:Article):
+        if len(obj.text) > 42:
+            return f"{obj.text[:42]}..."
+        return obj.text
+    list_display = "id", "title", "text_short", "time_to_read", "ts_created", "ts_last_changed",  "author"
+    list_display_links = "title", "text_short"
