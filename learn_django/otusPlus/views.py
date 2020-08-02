10575f5c3d8d85d8bd94b492092e79af279d502a
xaldey <xaldey@mail.ru>
2020-08-02 17:07:42 +0300

    Необходима правка "Ближайших уроков" (не удается настроить фильтр даты)

--- learn_django/otusPlus/views.py
+++ learn_django/otusPlus/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 from .models import Author, Document, Course, Student, Teacher, Lesson
 
 
 def index_view(request):
     # return HttpResponse('<h1>Hello, OtusPlus index!</h1>')
-
+    right_now = datetime.now()
     document = Document.objects.get(pk=1)
     context = {
         'foo': 'bar',
@@ -16,6 +17,7 @@ def index_view(request):
         'all_courses': Course.objects.all(),
         'all_teachers': Teacher.objects.all(),
         'all_lessons': Lesson.objects.all(),
+        'hot_lessons': Lesson.objects.all()
 
     }
     return render(request, 'otusPlus/index.html', context=context)
